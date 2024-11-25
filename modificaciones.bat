396a7a34ae041f042df453afbe1f1552888624de
fidel70 <fid.l.mori70@gmail.com>
2024-11-25 15:16:01 -0500

    your commit message

--- modificaciones.bat
+++ modificaciones.bat
@@ -1,5 +1,4 @@
 git add .
-git commit -m "fix: duplicated joinGame event"
-git remote add origin https://github.com/fidel70/ajedrez-online.git
-git push -u origin master
+git commit -m "your commit message"
+git push
 pause
